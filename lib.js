3a5fe8ca8a2da0a9b17a6553aa19fbb6846b1d55
Andrew Speakman <andrew@speakman.org.uk>
2020-12-30 11:04:09 +0000

    Update lib.js

--- lib.js
+++ lib.js
@@ -1,18 +1,28 @@
 // Function library
+process.env.NODE_ENV = "local_default"; // use local settings as default
 const config = require('config');
 
 function enableCors (req, res) {
-        /*if (req.headers['access-control-request-method']) {
-                res.setHeader('access-control-allow-methods', req.headers['access-control-request-method']);
+    if (!config.has('corsAllow')) return;
+    const corsAllow = config.get('corsAllow');
+    if (req.headers['access-control-request-method']) {
+            res.setHeader('access-control-allow-methods', 'POST, GET, OPTIONS'); // allow read only access
+    }
+    if (req.headers['access-control-request-headers']) {
+            res.setHeader('access-control-allow-headers', req.headers['access-control-request-headers']); // agree to any request header
+    }
+    res.setHeader('access-control-allow-credentials', 'false');
+    if (req.headers.origin && Array.isArray(corsAllow))  {
+        for (const origin_match of corsAllow) {
+            if (req.headers.origin.match(origin_match)) { // does the pattern match this origin
+                res.setHeader('access-control-allow-origin', req.headers.origin);
+                res.setHeader('vary', 'Origin');
+                break;
+            }
         }
-        if (req.headers['access-control-request-headers']) {
-                res.setHeader('access-control-allow-headers', req.headers['access-control-request-headers']);
-        }
-        if (req.headers.origin) {
-            res.setHeader('access-control-allow-origin', req.headers.origin);
-            res.setHeader('access-control-allow-credentials', 'true');
-        }*/
-        res.setHeader('Access-Control-Allow-Origin', '*');
+    } else {
+        res.setHeader('access-control-allow-origin', corsAllow);
+    }
 };
 
 function jsonToGeoJSON (body) {
