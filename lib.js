79f9a34baa84b6498f61c27e019d168556b06b1a
Andrew Speakman <andrew@speakman.org.uk>
2021-01-08 05:54:14 +0000

    OpenAPI includes POST for rpc methods

--- lib.js
+++ lib.js
@@ -95,8 +95,7 @@ function jsonToGeoJSON (body) {
 }
 
 function openAPIJSON (body) {
-    //const read_methods = [ 'get', 'post', 'options' ];
-    const read_methods = [ 'get' ]; // get only as the others are confusing
+    const read_methods = [ 'get', 'post' ];
     for (var p in body.paths) { // path keys
         for (var m in body.paths[p]) { // method keys
             if (read_methods.indexOf(m) < 0) {
